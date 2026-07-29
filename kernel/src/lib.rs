3c0590f17c4238a408b1a1725ecfc2e8777b7c2d
14elias <ellamebrahtom1995@gmail.com>
2026-07-29 21:24:07 +0300

    Improve structured CLI output

--- kernel/src/lib.rs
+++ kernel/src/lib.rs
@@ -8,3 +8,10 @@ pub mod space;
 mod sources;
 mod sinks;
 mod pure;
+<<<<<<< Updated upstream
+=======
+pub mod output;
+
+pub use sinks::WriteResourceRequest;
+pub use sources::ResourceRequest;
+>>>>>>> Stashed changes
