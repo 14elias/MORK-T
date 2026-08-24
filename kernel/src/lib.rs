75fb295385e553034cca6270583c6857d60f2c02
14elias <ellamebrahtom1995@gmail.com>
2026-08-24 14:22:03 +0300

    feat: integrate Python execution with MORK

--- kernel/src/lib.rs
+++ kernel/src/lib.rs
@@ -4,10 +4,14 @@
 #![feature(stmt_expr_attributes)]
 #![feature(more_float_constants)]
 
-pub mod space;
-mod sources;
-mod sinks;
 mod pure;
+pub mod python;
+pub mod python_manager;
+mod sinks;
+mod sources;
+pub mod space;
 
+pub use python::{PyCommand, PyQuery, PyResult, PyValue};
+pub use python_manager::{PyIpcError, PySessionManager, PyWorkerProcess};
 pub use sinks::WriteResourceRequest;
 pub use sources::ResourceRequest;
